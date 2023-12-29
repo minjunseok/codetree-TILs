8f946e625845c26f07b3fa0819ab3842a4c02970
minjunseok <150895656+minjunseok@users.noreply.github.com>
2023-12-29 16:16:21 +0900

    Title: 변수 선언하기, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

--- 231229/변수 선언하기/declaring-variables.java
+++ 231229/변수 선언하기/declaring-variables.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
    int a = 3;
     char b = 'C';
-   System.out.println(a);
-   System.out.println(b);
+   System.out.printf("%d\n",a);
+   System.out.printf("%c",b);
     }
 }
