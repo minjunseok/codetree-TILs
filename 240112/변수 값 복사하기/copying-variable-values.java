914e8a59ffe0079fc0347a022415226cb9682859
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-01-12 11:10:27 +0900

    Title: 변수 값 복사하기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240112/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1 , b = 2, c = 3 ;
+        
+        a = b = c;
+
+        System.out.print(a);
+        System.out.print(" ");
+        System.out.print(b);
+        System.out.print(" ");
+        System.out.print(c);
+    }
+}
