bc1eb52ed80b15982215dbf6c242e014cc71cfd6
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-01-15 09:12:56 +0900

    Title: 합을 복사하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240115/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+   int a = 1 , b = 2 , c = 3;
+
+   int temp = a + b + c;
+
+   a = b = c = temp;
+
+    System.out.print(a + " ");
+    System.out.print(b + " ");
+    System.out.print(c + " ");
+
+   }
+}
