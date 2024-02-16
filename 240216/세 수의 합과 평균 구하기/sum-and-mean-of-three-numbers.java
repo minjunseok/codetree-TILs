f0eed820c3d6ebd75ffba1192e4b46f526b36b96
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-02-16 09:48:01 +0900

    Title: 세 수의 합과 평균 구하기, Time: 151ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240216/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        System.out.printf("%.0f\n",a + b + c);
+        System.out.printf("%.0f",(a + b + c)/3);
+        // 여기에 코드를 작성해주세요.
+    }
+}
