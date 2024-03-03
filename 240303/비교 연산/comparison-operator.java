60fd4e9bfbf7b9fde0a905436aaab180df0458a2
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-03 23:53:34 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240303/비교 연산/comparison-operator.java
+++ 240303/비교 연산/comparison-operator.java
@@ -1,46 +1,19 @@
-import java.util.Scanner;
+import java.util.*
 
 public class Main {
     public static void main(String[] args) {
     
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
+    Scanner sc = new Scanner(System.in); //스캐너 객체 생성
+    int a = sc.nextInt(); //스캐너를 통한 입력 변수 a와 b 선언
     int b = sc.nextInt();
 
-    if (a >= b) {
-        System.out.println(1);
-    } else {
-        System.out.println(0);
-    }
-    if (a > b) {
-        System.out.println(1);
-    } else { 
-        System.out.println(0);
-    }
 
-    if (b >= a) {
-        System.out.println(1);
-    } else { 
-        System.out.println(0);
-    }
-
-    if (b > a) {
-        System.out.println(1);
-    } else { 
-        System.out.println(0);
-    }
-
-    if (a == b) {
-        System.out.println(1);
-    } else { 
-        System.out.println(0);
-    }
-
-    if (a != b) {
-        System.out.print(1);
-    } else { 
-        System.out.print(0);
-    }
-    
+        //기존의 if 문이 아닌 삼항연산자를 조건들을 이용해 출력시킨다.
+    System.out.println(a >= b ? 1 : 0);
+    System.out.println(a > b ? 1 : 0);
+    System.out.println(b >= a ? 1 : 0);
+    System.out.println(b > a ? 1 : 0);
+    System.out.println(a == b ? 1 : 0);
+    System.out.println(a != b ? 1 : 0);
     }
 }
