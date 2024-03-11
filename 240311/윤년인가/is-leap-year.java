3a9b0c3c8acfba3c107c46e1a4dbed7025c60643
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-11 15:06:49 +0900

    Title: 윤년인가, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240311/윤년인가/is-leap-year.java
+++ 240311/윤년인가/is-leap-year.java
@@ -5,9 +5,10 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     if (a  % 4 == 0) {
+        System.out.print("true");
         if ((a % 100 == 0) && (a % 400 == 0)) {
         System.out.print("true");
-        } else {
+        } else if ((a % 100 == 0) || (a % 400 == 0)) {
         System.out.print("false");
     }
     }
