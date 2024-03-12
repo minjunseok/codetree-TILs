9fe5cdf9810dca540097a4e735b2d47089dcc494
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-12 23:31:52 +0900

    Title: 윤년인가, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/윤년인가/is-leap-year.java
+++ 240312/윤년인가/is-leap-year.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
+
+
+    //주석 안 코드는 내가 한참동안 붙잡고 놓아주질 않았던 코드로 계속 특정 반례를 통과시키지 못 했다
+    //그래서 아예 false인 경우를 먼저 생각하고 아래쪽에 새로 작성했다.
 //     if (a  % 4 == 0) {
 //         System.out.print("true");
 //         } else if ((a % 100 == 0) && (a % 400 == 0)) {
