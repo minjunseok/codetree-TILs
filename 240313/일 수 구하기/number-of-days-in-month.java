cdbe0dd6b389ae72bc69477ad290d2ceb11042c4
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-13 10:44:36 +0900

    Title: 일 수 구하기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/일 수 구하기/number-of-days-in-month.java
+++ 240313/일 수 구하기/number-of-days-in-month.java
@@ -4,16 +4,18 @@ public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
- 
-       
-    if (a == 1 || (a % 2 != 0) && ((a >= 8) && (a % 2 == 0))) {
-        //1,3,5,7,8,10,12
+    if (a == 2) {
+        System.out.print(28);
+    } else if ((a == 1) || (a <= 7 && a % 2 != 0) || (a >= 8 && a % 2 == 0)) {
         System.out.print(31);
-    } else if (a == 2) {
-         System.out.print(28);
     } else {
-        //4,6,9,11
         System.out.print(30);
-    }     
+    }
     }
 }
+       
+        //1,3,5,7,8,10,12 = 31
+    
+        //2 = 28
+  
+        //4,6,9,11 = 30
