8dac3faf2573837809ca1140e40cfc9620949ab0
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-17 18:47:53 +0900

    Title: 증가시키며 출력하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240317/증가시키며 출력하기/increase-and-print.java
+++ 240317/증가시키며 출력하기/increase-and-print.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-    int a = 0;
-
     for(int i = 5; i <= 17; i += 2) {
-       a = i; 
-       System.out.print(a + " ");
+       System.out.print(i + " ");
     }
 
     }
