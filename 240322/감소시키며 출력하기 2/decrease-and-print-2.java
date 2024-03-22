897713602e15b624dddc41ac3530325831806c09
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-22 10:34:26 +0900

    Title: 감소시키며 출력하기 2, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240322/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    int i = 26;
+    int a = 10;
+
+
+    while (a <= i) {
+        System.out.print(i + " ");
+        i -= 1;
+    }
+
+    }
+}
