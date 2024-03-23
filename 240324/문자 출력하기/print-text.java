be343b6b8fb35e09414b1ad8bb0d505782ee86da
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-24 00:31:40 +0900

    Title: 문자 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240324/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.nextLine();
+
+    char b = a.charAt(0);
+
+    for(int i = 0; i < 8; i++) {
+        System.out.print(b);
+    }
+    }
+}
