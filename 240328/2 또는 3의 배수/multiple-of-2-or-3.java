afb8872d0f3939240128845311fac94383ac64cb
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-28 11:33:38 +0900

    Title: 2 또는 3의 배수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240328/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    for(int i = 1; i <= a; i++) {
+        if (i % 2 == 0 || i % 3 == 0) {
+            System.out.print(1 + " ");
+    } else {
+        System.out.print(0 + " ");
+    }
+    }
+}
+}
