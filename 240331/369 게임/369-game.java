d250fcec61ad2539bfc009854cc0edd5472719dd
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-31 23:01:54 +0900

    Title: 369 게임, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240331/369 게임/369-game.java
+++ 240331/369 게임/369-game.java
@@ -8,13 +8,12 @@ public class Main {
 
     for(int i = 1; i <= a; i++){
         
-        int c = i / 10;
+        
         int d = i % 10; 
+        int c = i / 10;
+        int f = i % 3;
 
-        if (c == 3 || c == 6 || c == 9) {
-            System.out.print(0 + " ");
-        }
-        if (d == 3 || d == 6 || d == 9)  {
+        if ((c == 3 || c == 6 || c == 9) || (d == 3 || d == 6 || d == 9) || (f == 0)) {
             System.out.print(0 + " ");
         } else { 
             System.out.print(i + " ");
