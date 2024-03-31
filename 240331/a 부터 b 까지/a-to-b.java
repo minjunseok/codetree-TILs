9cbff2351390467fa0d2966ca5db931a86d08288
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-03-31 21:24:02 +0900

    Title: a 부터 b 까지, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240331/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+
+    for(int i = a; i < b;) {
+    if (i % 2 == 0 && i < b) {
+        System.out.print(i + " ");
+        i += 3;
+    }
+    if (i % 2 != 0 && i < b) {
+        System.out.print(i + " ");
+        i = i * 2;
+    }
+    }
+
+    }
+}
