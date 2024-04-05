756eeddb423fd1cb5d36318a897b242372aeef34
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-04-05 09:33:55 +0900

    Title: a부터 b까지의 합, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240405/a부터 b까지의 합/sum-from-a-to-b.java
+++ 240405/a부터 b까지의 합/sum-from-a-to-b.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int sumVal = 0;
+    int sum = 0;
 
     int a = sc.nextInt();
     int b = sc.nextInt();
 
     for(int i = a; i <= b; i++) {
-        sumVal += i;
+        sum += i;
     }
-    System.out.print(sumVal);
+    System.out.print(sum);
     }
 }
