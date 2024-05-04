f9b92d49faceeacf25fad42d28206488a35935ac
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-05-04 22:49:12 +0900

    Title: 1까지 나누기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240504/1까지 나누기/divide-by-1.java
+++ 240504/1까지 나누기/divide-by-1.java
@@ -7,21 +7,19 @@ public class Main {
     int a = sc.nextInt();
     int dev = 0;
     int cnt = 0;
+    dev = a;
 
     for(int i = 1; i <= a; i++) {
 
-      dev = a / i;
-      cnt++;
-
-        if(dev > 1) {
-            continue;
         
-        } else {
-            System.out.print(cnt);
-            break;
-          
+        dev /= i;
+        cnt++;
+
+        if(dev <= 1) {
+           break;
         }
 
     }
+    System.out.print(cnt);
     }
 }
