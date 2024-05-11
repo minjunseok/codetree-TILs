1525f41d42102442399a561d274c31f523d377a7
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-05-11 11:44:31 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240511/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int cnt = 0;
+        double avr = 0;
+
+    while(true) {
+
+        int a = sc.nextInt();
+
+        if(a < 30) {
+
+            sum += a;
+            cnt++;
+
+        } else {
+
+            avr = (double) sum / cnt;
+            System.out.printf("%.2f" , avr);
+            break;
+        }
+
+        
+    }
+
+    sc.close();
+
+    }
+
+}
