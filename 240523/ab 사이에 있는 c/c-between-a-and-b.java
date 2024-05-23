28959eb9e93ece7ca69bb600f27a024823e12973
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-05-23 15:51:10 +0900

    Title: ab 사이에 있는 c, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240523/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+    
+        for (int i = a; i <= b i++;) {
+
+            if (c % a == 0) {
+                System.out.print("YES");
+                break;
+            } else if (c % b == 0) {
+                System.out.print("YES");
+                break;
+            } else {
+                System.out.print("NO");
+                break;
+            }
+        }
+        
+        }
+    }
