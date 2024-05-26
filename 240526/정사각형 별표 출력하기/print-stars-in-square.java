f2059e6d4a76d1ccc8f72824e5895713ac42d395
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-05-26 21:32:29 +0900

    Title: 정사각형 별표 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240526/정사각형 별표 출력하기/print-stars-in-square.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for(int i = 0; i < a; i++) {
+
+            for(int l = 0; l < a; l++) {
+
+                System.out.print("*");
+                
+            }
+                System.out.println();
+
+        }
+    }
+}
