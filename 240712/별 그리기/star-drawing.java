05260cd43487274ac32866f6a586a23f8e66b2b5
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-07-12 16:23:59 +0900

    Title: 별 그리기, Time: 401ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240712/별 그리기/star-drawing.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            
+            for (int j = i; j < n; j++) {
+
+                System.out.print(" ");
+
+            }
+
+                for(int j = 0; j < 2 * i - 1; j++) {
+                
+                    System.out.print("*");
+
+                }
+
+            System.out.println();
+
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+}
+}
