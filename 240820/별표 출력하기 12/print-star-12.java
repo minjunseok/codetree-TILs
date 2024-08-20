e3cd637d320ea19a24dfb35e9fa232ac03400435
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-08-20 21:31:24 +0900

    Title: 별표 출력하기 12, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240820/별표 출력하기 12/print-star-12.java
+++ 240820/별표 출력하기 12/print-star-12.java
@@ -7,30 +7,18 @@ public class Main {
 
         int n = sc.nextInt();
 
-
-        for(int i = 0; i < n; i++) {
-
-
-            System.out.print("* ");
-
-           for(int j = 0; j < n; j++) {
-
-            if(i > 0 && j % 2 == 0 && j < i) {
-
-                System.out.print("* ");
-
-            } else {
-
-                System.out.print(" ");
+        for(int i = 0 ; i<n; i++){
+            
+            for(int j=0; j<n; j++){
+            if( (i>0&&j%2==0) || j<i){
+                System.out.print("  ");
                 
-                            }
-
-
-           }
-
-            System.out.println();
-
-
+            }
+            else if( i==0 || (j%2==1)  ) {
+                System.out.print("* ");
+            }
+            }
+        System.out.println();
         }
     }
 }
