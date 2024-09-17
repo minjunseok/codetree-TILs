c0d4a70cdad03dc03ee390e31ba605db50c51f58
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-09-17 18:55:21 +0900

    Title: 홀수로 이루어진 정사각형, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/홀수로 이루어진 정사각형/square-with-odd-numbers.java
+++ 240917/홀수로 이루어진 정사각형/square-with-odd-numbers.java
@@ -23,7 +23,8 @@ public class Main {
                 }
 
                 System.out.println();
-                cnt -= 8;
+                cnt -= (n * 2) - 2;
+                
                 
             }
 
