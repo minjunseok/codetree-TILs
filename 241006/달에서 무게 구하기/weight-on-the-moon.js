402104198701c1be3299efafffb10bc740f33491
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-10-06 15:50:30 +0900

    Title: 달에서 무게 구하기, Time: 168ms, Memory: 14MB, Status: Passed - CodeTree

--- 241006/달에서 무게 구하기/weight-on-the-moon.js
+++ 241006/달에서 무게 구하기/weight-on-the-moon.js
@@ -2,5 +2,15 @@ let weight = 13;
 
 let gravity = 0.165;
 
+let result = weight * gravity;
 
-console.log(weight , "*" , gravity.toFixed(6), "=" , (weight * gravity).toFixed(6));
+
+
+//내가 적은 답
+
+// console.log(weight , "*" , gravity.toFixed(6), "=" , (weight * gravity).toFixed(6));
+
+
+//해설에 나온 답
+
+console.log(`${weight} * ${gravity.toFixed(6)} = ${result.toFixed(6)}`);
