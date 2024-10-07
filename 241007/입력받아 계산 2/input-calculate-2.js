3c170f1fd8e4b297f8d5795961bc5b8c252c0676
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-10-07 17:53:32 +0900

    Title: 입력받아 계산 2, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 241007/입력받아 계산 2/input-calculate-2.js
+++ 241007/입력받아 계산 2/input-calculate-2.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 
-let a = fs.readFileSync(0).toString();
 
-let arr = a.split(" ");
+// 내가 적은 정답
+
+// let a = fs.readFileSync(0).toString();
+
+// let arr = a.split(" ");
 
 
 
-console.log(arr[0] * arr[1]);
+// console.log(arr[0] * arr[1]);
+
+
+
+// 문자열을 특정 기준으로 잘라주는 함수 잘려나간 단위가 하나의 원소가 되어 
+// 원소들의 값을 가진 하나의 배열이 만들어진다.
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+
+let b = Number(input[1]);
+
+console.log(a * b);
