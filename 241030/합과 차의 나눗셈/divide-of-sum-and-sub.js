4cb8335403bf6c10a3c52766106d44b346fd75a1
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-10-30 00:51:33 +0900

    Title: 합과 차의 나눗셈, Time: 217ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 241030/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+
+let b = Number(input[1]);
+
+let c = a + b;
+
+let d = a - b;
+
+console.log((c / d).toFixed(2));
