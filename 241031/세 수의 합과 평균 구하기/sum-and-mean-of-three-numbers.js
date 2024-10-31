a66d0eccfb4aa395e432eab04e3c11f790183cfb
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-10-31 14:22:31 +0900

    Title: 세 수의 합과 평균 구하기, Time: 212ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 241031/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+
+let b = Number(input[1]);
+
+let c = Number(input[2]);
+
+let sum = a + b + c;
+
+let avr = parseInt((a + b + c) / 3);
+
+
+console.log(sum);
+
+console.log(avr);
