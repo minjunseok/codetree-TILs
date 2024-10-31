ff0b1b6968e29cf5faa4bc93c88adee9e1c0cede
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-10-31 14:29:03 +0900

    Title: 합과 평균의 차, Time: 177ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241031/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+
+let b = Number(input[1]);
+
+let c = Number(input[2]);
+
+let sum = a + b + c;
+
+let avr = parseInt((a + b + c) / 3);
+
+let min = sum - avr;
+
+
+console.log(sum);
+
+console.log(avr);
+
+console.log(min);
