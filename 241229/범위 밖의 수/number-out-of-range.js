12caee51cf7edbb5e0a62e51f7799632d68db69b
minjunseok <150895656+minjunseok@users.noreply.github.com>
2024-12-29 21:32:52 +0900

    Title: 범위 밖의 수, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241229/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+
+let a =  Number(input[0]);
+
+
+if(a < 10 || a > 20) {
+
+    console.log("yes");
+
+} else {
+
+    console.log("no");
+    
+}
